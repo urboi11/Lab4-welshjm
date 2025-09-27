09dca796697648565fd11a6d49459943b644893b
Jason Welsh <welshjason03@gmail.com>
2025-09-27 14:59:04 -0400

    Adding of project files

--- src/main/java/com/lab4/assignment/Addresses.java
+++ src/main/java/com/lab4/assignment/Addresses.java
@@ -8,10 +8,12 @@ import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Entity
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Addresses {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
--- src/main/java/com/lab4/assignment/Profiles.java
+++ src/main/java/com/lab4/assignment/Profiles.java
@@ -4,10 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profiles {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
